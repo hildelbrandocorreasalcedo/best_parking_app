2f690f2587117262d6bce8ce731276e072ac2489
Hildelbrando Correa Salcedo <hscorrea@unicesar.edu.co>
2022-05-31 01:54:25 -0500

    cambios

--- lib/models/vehiculo.dart
+++ lib/models/vehiculo.dart
@@ -1,17 +1,14 @@
-/*
 class Vehiculo {
   final String id_vehiculo;
   final String tipo;
   final String placa;
   final String marca;
-  final String foto_vehiculo;
 
   Vehiculo({
     required this.id_vehiculo,
     required this.placa,
     required this.tipo,
     required this.marca,
-    required this.foto_vehiculo,
   });
 
   factory Vehiculo.fromJson(Map<String, dynamic> json) {
@@ -20,8 +17,6 @@ class Vehiculo {
       tipo: json['tipo'],
       placa: json['placa'],
       marca: json['marca'],
-      foto_vehiculo: json['foto_vehiculo'],
     );
   }
 }
-*/
